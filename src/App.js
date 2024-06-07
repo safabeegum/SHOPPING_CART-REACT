3edc2894034fe01a26b0acb056d1875a7a4da828
safabeegum <pasafabeegum@gmail.com>
2024-06-08 00:55:30 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import AddItem from './components/AddItem';
 import SearchItem from './components/SearchItem';
 import DeleteItem from './components/DeleteItem';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewItem from './components/ViewItem';
 
 function App() {
   return (
-    <div>
-      <AddItem/>
-      <SearchItem/>
-      <DeleteItem/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddItem/>}/>
+      <Route path='/search' element={<SearchItem/>}/>
+      <Route path='/delete' element={<DeleteItem/>}/>
+      <Route path='/viewall' element={<ViewItem/>}/>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-warning">
+        <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <Link class="navbar-brand" to="#">ZOO</Link>
+    <Link class="navbar-brand" to="#">AMAZON</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <Link class="nav-link" to="/">ADD ITEMS</Link>
+          <Link class="nav-link" to="/">ADD ITEM</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" to="/search">SEARCH ITEMS</Link>
+          <Link class="nav-link" to="/search">SEARCH ITEM</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" to="/delete">DELETE ITEMS</Link>
+          <Link class="nav-link" to="/delete">DELETE ITEM</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" to="/viewall">VIEW ITEMS</Link>
+          <Link class="nav-link" to="/viewall">VIEW ITEM</Link>
         </li>
+        
       </ul>
     </div>
   </div>
