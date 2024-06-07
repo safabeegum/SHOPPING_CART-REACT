bb4353a24e981187d09340b28ca5b653d538f5ca
safabeegum <pasafabeegum@gmail.com>
2024-06-07 23:48:21 +0530

    Completed

---
+++ src/components/AddItem.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const AddItem = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PRODUCT ID</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PRODUCT NAME</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">DESCRIPTION</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PRICE</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">CATEGORY</label>
+                            <select name="" id="" className="form-control">
+                                <option value="Mammal">Fashion</option>
+                                <option value="Bird">Grocery</option>
+                                <option value="Reptile">Electronics</option>
+                                <option value="Fish">Food and Beverage</option>
+                                <option value="Amphibian">Home Furnishing</option>
+                            </select>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">MANUFACTURING DATE</label>
+                            <input type="date" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">EXPIRY DATE</label>
+                            <input type="date" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">BRAND</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">COLOR</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">SUCCESS</button>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-secondary">RESET</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddItem
