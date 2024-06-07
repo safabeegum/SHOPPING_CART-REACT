81af1151f5655e0075cc4d2680377b9462a66c6f
safabeegum <pasafabeegum@gmail.com>
2024-06-07 23:52:15 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddItem from './components/AddItem';
 
 function App() {
   return (
     <div>
-      
+      <AddItem/>
     </div>
   );
 }
